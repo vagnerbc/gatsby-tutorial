bc69296a705afa47a9caedda872e690cf15b18ac
Vagner Bernal de castro <vagner.castro@objective.com.br>
2020-09-10 23:37:13 -0300

    Create initial structure

---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from "typography"
+import KirthamTheme from "typography-theme-kirkham"
+
+const typography = new Typography(KirthamTheme)
+
+export default typography
+export const rhythm = typography.rhythm
