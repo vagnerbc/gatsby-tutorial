61e9d5ca9f72a72ce432b6c559afbabe06fb4ab6
Vagner Bernal de castro <vagner.castro@objective.com.br>
2020-09-15 23:17:37 -0300

    audit with lighthouse

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,26 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `Vagner B.C`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
